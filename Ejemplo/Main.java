dc4894a367049cfb94a4a9309311dfe4af525232
andre.1999-10 <andre.1999-10@hotmail.com>
2017-07-19 11:59:26 -0400

    resta, multiplicacion, division

--- Ejemplo/Main.java
+++ Ejemplo/Main.java
@@ -4,11 +4,40 @@ public class Main {
   
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
-    int s = suma(20, 30);
-    System.out.println(s);
+    int suma = suma(20, 30);
+    System.out.println(suma);
+    int resta = resta(15, 9);
+    System.out.println(resta);
+    int multiplicacion = multiplicacion(10,5);
+    System.out.println(multiplicacion);
+    
+    try {
+      double division = division(14,0);
+      System.out.println(division);
+    } catch(Exception e) {
+      String mensaje = e.getMessage();
+      System.out.println(mensaje);
+    }
+      
+   
   }
   
   public static int suma(int n1, int n2) {
-    return n1+ n2;
+    return n1 + n2;
+  }
+  
+  public static int resta(int n1, int n2) {
+    return n1 - n2;
+  }
+  
+  public static int multiplicacion(int n1, int n2) {
+    return n1 * n2;
+  }
+  
+  public static double division(double n1, int n2) throws Exception{
+    if(n2 ==0) {
+      throw new Exception("No es posible realizar division por cero");
+    }
+    return n1/n2;
   }
 }
